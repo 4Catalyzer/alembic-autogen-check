c5cf1bba7953cb5194178617c13372469eb642a4
Steven Loria <sloria1@gmail.com>
2019-05-12 23:41:44 -0400

    test: add tests

---
+++ tests/models.py
@@ -0,0 +1,17 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+OutOfSyncBase = declarative_base()
+
+
+class Widget(Base):
+    __tablename__ = "widgets"
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String, index=True)
+
+
+class WidgetUpdated(OutOfSyncBase):
+    __tablename__ = "widgets"
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
