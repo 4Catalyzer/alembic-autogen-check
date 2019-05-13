d1af286c78476d5f2d61d88fdeaf17cc8ed03c91
Steven Loria <sloria1@gmail.com>
2019-05-13 15:18:06 -0400

    test: add tests (#2)
    * test: add tests
    
    * Add spacing for readability

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,8 +1,11 @@
 import typing as t
+from pathlib import Path
 
 import pytest
 from alembic_autogen_check import main
 from click.testing import CliRunner
+
+config_file = Path(__file__).parent / "alembic.ini"
 
 
 @pytest.fixture()
@@ -20,4 +23,19 @@ def test_help(run_command):
     assert result.exit_code == 0
 
 
-# TODO: Test the actual command
+def test_version(run_command):
+    result = run_command("--version")
+    assert result.exit_code == 0
+
+
+def test_in_sync_exits_with_success(run_command):
+    result = run_command(["--config", str(config_file)])
+    assert result.exit_code == 0
+    assert "INFO: Migrations in sync." in result.output
+
+
+def test_out_of_sync_exits_with_error(run_command, monkeypatch):
+    monkeypatch.setenv("ALEMBIC_OUT_OF_SYNC", "true")
+    result = run_command(["--config", str(config_file)])
+    assert result.exit_code == 1
+    assert "ERROR: Migrations are out of sync with models." in result.output
