96da2260f7bf337a66d13d18ea34db4ca3768d5e
Steven Loria <sloria1@gmail.com>
2019-05-13 15:17:44 -0400

    Add spacing for readability

--- tests/models.py
+++ tests/models.py
@@ -7,11 +7,13 @@ OutOfSyncBase = declarative_base()
 
 class Widget(Base):
     __tablename__ = "widgets"
+
     id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.String, index=True)
 
 
 class WidgetUpdated(OutOfSyncBase):
     __tablename__ = "widgets"
+
     id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.String)
